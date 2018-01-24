e092d664b2e7428e3dcc4bf7184b7ed2d5e97f72
Robert Scheffel <robert.scheffel@commsolid.com>
2018-01-24 16:43:03 +0100

    introduce class for model

--- extparser.py
+++ extparser.py
@@ -4,23 +4,59 @@ import clang.cindex
 import sys
 
 
-def method_definitions(node):
-    for child in node.get_children():
-        method_definitions(child)
+class Model:
 
-    if node.kind == clang.cindex.CursorKind.COMPOUND_STMT:
-        extract_definition(node)
+    def __init__(self):
+        index = clang.cindex.Index.create()
+        tu = index.parse(
+            sys.argv[1], ['-x', 'c++', '-std=c++11'])
+
+        self._name = ""
+        self._dfn = ""
+
+        self.method_name(tu.cursor)
+        self.method_definitions(tu.cursor)
+
+    def method_name(self, node):
+        for child in node.get_children():
+            self.method_name(child)
+
+        if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+            # print(node.spelling)
+            self._name = node.spelling
+
+    def method_definitions(self, node):
+        for child in node.get_children():
+            self.method_definitions(child)
+
+        if node.kind == clang.cindex.CursorKind.COMPOUND_STMT:
+            self.extract_definition(node)
+
+    def extract_definition(self, node):
+        filename = node.location.file.name
+        with open(filename, 'r') as fh:
+            contents = fh.read()
+        # print(contents[node.extent.start.offset: node.extent.end.offset])
+        self._dfn = contents[node.extent.start.offset: node.extent.end.offset]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def definition(self):
+        return self._dfn
 
 
-def extract_definition(node):
-    filename = node.location.file.name
-    with open(filename, 'r') as fh:
-        contents = fh.read()
-    print(contents[node.extent.start.offset: node.extent.end.offset])
+def parse_model():
+    model = Model()
+
+    name = model.name
+    print(name)
+
+    dfn = model.definition
+    print(dfn)
 
 
-index = clang.cindex.Index.create()
-tu = index.parse(
-    sys.argv[1], ['-x', 'c++', '-std=c++11'])
-
-method_definitions(tu.cursor)
+if __name__ == '__main__':
+    parse_model()
