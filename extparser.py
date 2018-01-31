8046328945d544c0dfae6d9fe422b3dedf9f75c4
Robert Scheffel <robert.scheffel@commsolid.com>
2018-01-31 14:02:26 +0100

    introduce new class instructions

--- extparser.py
+++ extparser.py
@@ -56,6 +56,70 @@ class Model:
         return self._dfn
 
 
+class Instructions:
+    '''
+    Has all necessary information about the custom instructions
+    that is needed to extend the RISC-V assembler.
+    '''
+
+    def __init__(self, models):
+        self._models = models
+        self._names = [model.name for model in models]
+
+        self.generate_opc()
+
+    def generate_opc(self):
+        opcodes_cust = Template(filename='opcodes-cust.mako')
+
+        opc_cust = 'opcodes-cust'
+        with open(opc_cust, 'w') as fh:
+            fh.write(opcodes_cust.render(models=self._models))
+
+        with open(opc_cust, 'r') as fh:
+            ops = fh.read()
+
+        try:
+            os.remove(opc_cust)
+        except OSError:
+            pass
+
+        p = subprocess.Popen(['riscv-opcodes/parse-opcodes',
+                              '-c'],
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        defines, _ = p.communicate(input=ops)
+
+        d = '\n'
+        lines = [e + d for e in defines.split(d)]
+
+        self._masks = [
+            entry for entry in lines if entry.startswith('#define MASK')]
+        self._matches = [
+            entry for entry in lines if entry.startswith('#define MATCH')]
+
+        assert len(self._masks) == len(
+            self._matches), 'Length of mask and match arrays differ'
+
+        print(self._masks)
+        print(self._matches)
+
+    @property
+    def models(self):
+        return self._models
+
+    @property
+    def names(self):
+        return self._names
+
+    @property
+    def masks(self):
+        return self._masks
+
+    @property
+    def matches(self):
+        return self._matches
+
+
 def parse_model():
     '''
     Parse the c++ reference implementation
@@ -71,49 +135,30 @@ def create_opcode(models):
     Create a temporary file from which the opcode
     of the custom instruction is going to be generated.
     '''
+    instructions = Instructions(models)
 
-    opcodes_cust = Template(filename='opcodes-cust.mako')
+    masks = instructions.masks
+    matches = instructions.matches
 
-    opc_cust = 'opcodes-cust'
-    with open(opc_cust, 'w') as fh:
-        fh.write(opcodes_cust.render(models=models))
+    # files that needs to be edited
+    opch = 'riscv-gnu-toolchain/riscv-binutils-gdb/include/opcode/riscv-opc.h'
+    opcc = 'riscv-gnu-toolchain/riscv-binutils-gdb/opcodes/riscv-opc.c'
 
-    with open(opc_cust, 'r') as fh:
-        ops = fh.read()
-
-    try:
-        os.remove(opc_cust)
-    except OSError:
-        pass
-
-    p = subprocess.Popen(['riscv-opcodes/parse-opcodes',
-                          '-c'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-    defines, _ = p.communicate(input=ops)
-
-    d = '\n'
-    lines = [e + d for e in defines.split(d)]
-
-    masks = [entry for entry in lines if entry.startswith('#define MASK')]
-    matches = [entry for entry in lines if entry.startswith('#define MATCH')]
-
-    assert len(masks) == len(matches), 'Length of mask and match arrays differ'
-
-    print(masks)
-    print(matches)
-
-    opc = 'riscv-gnu-toolchain/riscv-binutils-gdb/include/opcode/riscv-opc.h'
-    with open(opc, 'r') as fh:
+    # the mask and match defines has to be added in the header file
+    with open(opch, 'r') as fh:
         content = fh.readlines()
 
-    # first line number is 3
     for i in range(0, len(masks)):
+        # first line number is 3
         content.insert(i + 3, masks[i])
         content.insert(i + 3, matches[i])
 
-    with open(opc, 'w') as fh:
+    with open(opch, 'w') as fh:
         content = ''.join(content)
         fh.write(content)
+
+    # in the c file the asm instruction is defined
+    # build string that has to be inserted
 
 
 def main():
