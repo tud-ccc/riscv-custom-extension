8334fefc396507722116ebe3c04d6574da1815dd
Robert <robert.scheffel1@tu-dresden.de>
2018-02-15 14:51:41 +0100

    set libclang path depending on os

--- modelparsing/__init__.py
+++ modelparsing/__init__.py
@@ -1,5 +1,6 @@
 import clang.cindex
 import logging
+import platform
 
 # Set default logging handler to avoid "No handler found" warnings.
 try:  # Python 2.7+
@@ -11,6 +12,12 @@ except ImportError:
 
 logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+try:
+	(name, version, code) = platform.linux_distribution()
+except:
+	(name, version, code) = ('n/a', 'n/a', 'n/a')
 
-clang.cindex.Config.set_library_file('/usr/lib64/llvm/libclang.so')
-# clang.cindex.Config.set_library_file('/usr/lib/llvm-4.0/lib/libclang-4.0.so')
+if name == 'Ubuntu':
+	clang.cindex.Config.set_library_file('/usr/lib/llvm-4.0/lib/libclang-4.0.so')
+elif name == 'CentOS':
+	clang.cindex.Config.set_library_file('/usr/lib64/llvm/libclang.so')
