48623c340f91f3d5c0e3677875ba476448f238b6
Robert Scheffel <robert.scheffel@commsolid.com>
2018-02-21 09:26:00 +0100

    rename test script

--- modelparsing/__init__.py
+++ modelparsing/__init__.py
@@ -12,12 +12,15 @@ except ImportError:
 
 logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-try:
-	(name, version, code) = platform.linux_distribution()
-except:
-	(name, version, code) = ('n/a', 'n/a', 'n/a')
+if platform.system() == 'Linux':
+    (name, version, code) = platform.linux_distribution()
+else:
+    (name, version, code) = ('n/a', 'n/a', 'n/a')
 
 if name == 'Ubuntu':
-	clang.cindex.Config.set_library_file('/usr/lib/llvm-4.0/lib/libclang-4.0.so')
-elif name == 'CentOS':
-	clang.cindex.Config.set_library_file('/usr/lib64/llvm/libclang.so')
+    clang.cindex.Config.set_library_file(
+        '/usr/lib/llvm-4.0/lib/libclang-4.0.so')
+elif name == 'CentOS Linux':
+    clang.cindex.Config.set_library_file('/usr/lib64/llvm/libclang.so')
+else:
+    raise ValueError(name, 'Linux Version not supported.')
