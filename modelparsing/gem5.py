d8dff9c80eaf339b1533557cb42f9ed5f6023127
Robert Scheffel <robert.scheffel@commsolid.com>
2018-05-30 15:25:49 +0200

    template for timing python file

--- modelparsing/gem5.py
+++ modelparsing/gem5.py
@@ -168,7 +168,59 @@ ${hex(mdl.funct7)}: R32Op::${mdl.name}({${mdl.definition}}, IntCustOp);
         every custom instruction.
         '''
 
-        pass
+        timing_templ = Template(r"""<%
+%>\
+# === AUTO GENERATED FILE ===
+
+# Copyright (c) 2018 TU Dresden
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Robert Scheffel
+
+from m5.objects import *
+
+% for model in models:
+class MinorFUTiming${model.name.title()}(MinorFUTiming):
+    description = 'Custom${model.name.title()}'
+    srcRegsRelativeLats = [2]
+    extraCommitLat = ${model.cycles - 1}
+
+
+% endfor
+
+custom_timings = [
+% for model in models:
+    MinorFUTiming${model.name.title()}(),
+% endfor
+]
+""")
+
+        self._FUtimings = timing_templ.render(models=self._models)
+
+        print(self._FUtimings)
 
     @property
     def decoder(self):
